48b0591f2f16e2bac88fe6bfd80741a89a5c2707
Farjad Suhail <farjad.suhail@systemsltd.com>
2021-11-12 15:39:08 +0500

    basics before directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  
+  // external styling
+  //styleUrls: ['./app.component.css']
+
+  //inline styles
+  styles: [`
+  h3{
+    color:red;
+  }`
+]
 })
 export class AppComponent {
-  title = 'my-first-app';
+  //name = 'Farjad';
 }
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+
+
+// -> we can have inline templates and external template files
+// totally depends on us
+@Component({
+  //as element
+  selector: 'app-servers',
+  
+  //as attribute
+  //selector: '[app-servers]',
+  
+  //as by class
+  //selector: '.app-servers',
+  
+  //external template
+  //templateUrl: './servers.component.html',
+
+//-> inline template
+// template: `
+// <app-server></app-server>
+// <app-server></app-server>
+// `
+templateUrl: './servers.component.html'
+,
+styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TESTSERVER';
+
+  constructor() {
+    
+  setTimeout(() => {
+    this.allowNewServer = true;
+  }, 2000);
+   }
+
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value
+    console.log(event);
+  }
+}
